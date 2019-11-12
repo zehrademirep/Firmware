8ee502965a8eab4739b8200ab2851059fcba7806
kamilritz <kritz@ethz.ch>
2019-11-12 10:48:59 +0100

    Start updating CI analysis

--- Tools/ecl_ekf/analyse_logdata_ekf.py
+++ Tools/ecl_ekf/analyse_logdata_ekf.py
@@ -59,6 +59,13 @@ def analyse_ekf(
 
     control_mode, innov_flags, gps_fail_flags = get_estimator_check_flags(estimator_status)
 
+    innov_msg_name = get_innovation_message(ulog)
+    innov_var_msg_name = get_innovation_variance_message(ulog)
+    field_name = get_field_name_from_message_and_descriptor("estimator_innovation_variances","drag_innovation_variance")
+    print(innov_msg_name)
+    print(innov_var_msg_name)
+    print(field_name)
+
     sensor_checks, innov_fail_checks = find_checks_that_apply(
         control_mode, estimator_status,
         pos_checks_when_sensors_not_fused=pos_checks_when_sensors_not_fused)
@@ -131,6 +138,105 @@ def find_checks_that_apply(
     return sensor_checks, innov_fail_checks
 
 
+def get_output_tracking_error_message(ulog: ULog) -> str:
+    """
+    return the name of the message containing the output_tracking_error
+    :param ulog:
+    :return: str
+    """
+    msg_name_list = []
+    for elem in  ulog.data_list:
+        msg_name_list.append(elem.name)
+    if "ekf2_innovations" in msg_name_list:
+        return "ekf2_innovations"
+    elif "estimator_innovations" in msg_name_list:
+        return "estimator_status"
+    else:
+        raise(PreconditionError("Could not detect the message containing the output tracking error"))
+
+def get_innovation_message(ulog: ULog) -> str:
+    """
+    return the name of the innovation message (old: ekf2_innovations; new: estimator_innovations)
+    :param ulog:
+    :return: str
+    """
+    msg_name_list = []
+    for elem in  ulog.data_list:
+        msg_name_list.append(elem.name)
+    if "ekf2_innovations" in msg_name_list:
+        return "ekf2_innovations"
+    elif "estimator_innovations" in msg_name_list:
+        return "estimator_innovations"
+    else:
+        raise(PreconditionError("Could not detect any known innovation message"))
+
+def get_innovation_variance_message(ulog: ULog) -> str:
+    """
+    return the name of the innovation variance message
+    :param ulog:
+    :return: str
+    """
+    msg_name_list = []
+    for elem in  ulog.data_list:
+        msg_name_list.append(elem.name)
+    if "ekf2_innovations" in msg_name_list:
+        return "ekf2_innovations"
+    elif "estimator_innovation_variances" in msg_name_list:
+        return "estimator_innovation_variances"
+    else:
+        raise(PreconditionError("Could not detect any known innovation message"))
+
+def get_field_name_from_message_and_descriptor(message: str, field_descriptor: str) -> str:
+    """
+    return the actual field name (old or new) for a field descriptor
+    e.g. (old) message: ekf2_innovations; field_descriptor: magnetometer_innovations -> mag_innov
+    :param ulog:
+    :return:
+    """
+    msg_lookUp_dict = {
+        'ekf2_innovations' :
+            {
+                'magnetometer_innovation' : 'mag_innov',
+                'heading_innovation' : 'heading_innov',
+                'airspeed_innovation' : 'airspeed_innov',
+                'sideslip_innovation' : 'beta_innov',
+                'flow_innovation' : 'flow_innov',
+                'hagl_innovation' : 'hagl_innov',
+                'drag_innovation' : 'drag_innov',
+                'magnetometer_innovation_variance' : 'mag_innov_var',
+                'heading_innovation_variance' : 'heading_innov_var',
+                'airspeed_innovation_variance' : 'airspeed_innov_var',
+                'sideslip_innovation_variance' : 'beta_innov_var',
+                'flow_innovation_variance' : 'flow_innov_var',
+                'hagl_innovation_variance' : 'hagl_innov_var',
+                'drag_innovation_variance' : 'drag_innov_var'
+            },
+        'estimator_innovations' :
+            {
+                'magnetometer_innovation' : 'mag',
+                'heading_innovation' : 'heading',
+                'airspeed_innovation' : 'airspeed',
+                'sideslip_innovation' : 'beta',
+                'flow_innovation' : 'flow',
+                'hagl_innovation' : 'hagl',
+                'drag_innovation' : 'drag',
+            },
+        'estimator_innovation_variances' :
+            {
+                'magnetometer_innovation_variance' : 'mag',
+                'heading_innovation_variance' : 'heading',
+                'airspeed_innovation_variance' : 'airspeed',
+                'sideslip_innovation_variance' : 'beta',
+                'flow_innovation_variance' : 'flow',
+                'hagl_innovation_variance' : 'hagl',
+                'drag_innovation_variance' : 'drag'
+            }
+    }
+    field = msg_lookUp_dict.get(message).get(field_descriptor)
+    if field == None:
+        raise(PreconditionError("Could not find field in lookup table"))
+    else:
+        return field
 
 
 
